256cc5ad3cd9d12f18cbe81b95fe456c63620f0d
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-11-01 06:35:19 +0530

    Create Leay_year.py

---
+++ Leay_year.py
@@ -0,0 +1,12 @@
+#CHECKING LEAP YEAR
+year=2000
+if(year%4==0):
+  if(year%100==0):
+    if(year%400==0):
+      print("leap year")
+    else:
+      print("Not a leap year")
+  else:
+    print("leap year")
+else:
+  print("Not a leap year")
