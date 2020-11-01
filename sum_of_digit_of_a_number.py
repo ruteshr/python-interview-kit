0fe5502dff37ef2e5856e7546effb71fdf768e95
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-11-01 10:03:32 +0530

    Create sum_of_digit_of_a_number.py

---
+++ sum_of_digit_of_a_number.py
@@ -0,0 +1,28 @@
+#SUM OF DIGITS OF A NUMBER
+n=235
+sum=0
+while(n>0):
+  temp=n%10
+  sum+=temp
+  n=n//10
+print(sum)
+'''
+OUTPUT
+  10
+'''
+
+
+#ADD THE RESULTANT OF THE ADDITION OF TWO NUMBER
+a=23
+b=34
+n=a+b
+sum=0
+while(n>0):
+  temp=n%10
+  sum+=temp
+  n=n//10
+print(sum)
+'''
+OUTPUT
+  12
+'''
