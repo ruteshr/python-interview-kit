708da95ca1f773013b8ec93de6fb349feb8f6f26
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-11-01 10:09:54 +0530

    Create swapping.py

---
+++ swapping.py
@@ -0,0 +1,23 @@
+#SWAPPING WITHOUT THIRD VARIABLE
+a=2
+b=3
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+'''
+OUTPUT
+  3 2
+'''
+
+#SWAPPING WITH THIRD VARIABLE
+a=8
+b=6
+temp=a
+a=b
+b=temp
+print(a,b)
+'''
+OUTPUT
+  6 8
+'''
