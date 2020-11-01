6e50db28d161a800057e486aee2189518062c2c7
ruteshr <44744748+ruteshr@users.noreply.github.com>
2020-11-01 11:33:59 +0530

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,41 @@
+#FIBONACCI USING RECURSION
+def fibonacci(n):
+  if(n<0):
+    print("Wrong Input")
+  elif(n==1):
+    return 0
+  elif(n==2):
+    return 1
+  else:
+    return fibonacci(n-1)+fibonacci(n-2)
+n=9
+for i in range(n+1):
+  print(fibonacci(n))
+'''
+OUTPUT
+  0 1 1 2 3 4 8 13 21
+'''
+
+
+#FIBONACCI WITHOUT RECURSION
+n=9
+a=0
+b=1
+if(n<=0):
+  print("wrong input")
+elif(n==1):
+  print(a)
+elif(n==2):
+  print(a,b)
+else:
+  print(a,b,end=" ")
+  while(n-2):
+    c=a+b
+    a=b
+    b=c
+    print(c,end=" ")
+    n-=1
+'''
+OUTPUT
+  0 1 1 2 3 4 8 13 21
+'''
